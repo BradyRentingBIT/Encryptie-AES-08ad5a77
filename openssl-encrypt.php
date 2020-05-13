c4d0254f6eac31d62addf62f1e48f423c405795f
iBrady <brady.renting@live.nl>
2020-05-13 15:34:59 +0200

    Blast issues fixed

--- openssl-encrypt.php
+++ openssl-encrypt.php
@@ -14,12 +14,10 @@ echo PHP_EOL . "Encrypting message.." . PHP_EOL;
 sleep(2);
 
 $cipher = "aes-128-gcm";
-if (in_array($cipher, openssl_get_cipher_methods()))
-{
+if (in_array($cipher, openssl_get_cipher_methods())) {
     $ivlen = openssl_cipher_iv_length($cipher);
     $iv = openssl_random_pseudo_bytes($ivlen);
-    $ciphertext = openssl_encrypt($plaintext, $cipher, $key, $options=0, $iv, $tag);
-    //store $cipher, $iv, and $tag for decryption later
-    $original_plaintext = openssl_decrypt($ciphertext, $cipher, $key, $options=0, $iv, $tag);
+    $ciphertext = openssl_encrypt($plaintext, $cipher, $key, $options = 0, $iv, $tag);
+    $original_plaintext = openssl_decrypt($ciphertext, $cipher, $key, $options = 0, $iv, $tag);
     echo "Successfully encrypted message '" . $original_plaintext . "' to '" . $ciphertext . "'" . PHP_EOL;
 }
